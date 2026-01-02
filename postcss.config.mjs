6745a2b663a790bbe62cb82a528f4b0f111a95ca
rajnish003 <raj003se.com>
2026-01-02 16:07:12 +0530

    Fix Tailwind PostCSS config and reinstall deps

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss",],
-  // postcss.config.js
-
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
